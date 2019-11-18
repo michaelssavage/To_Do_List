96072e99492dcf6652cd09ea96c356c44f6c8c7d
Michael S <33690140+michaelssavage@users.noreply.github.com>
2019-11-18 16:07:25 +0000

    Update test.py

--- oop/test.py
+++ oop/test.py
@@ -10,29 +10,38 @@ def main():
 	q.enqueue(Task("Buy milk","12/1/2039","11:00","4.30hrs",["John","Mary","Ben"]))
 	q.enqueue(Event("New Years","1/1/2020","12:00","Dublin"))
 
-	print("Show all jobs: ")
+	print("1. Show all jobs: ")
+	#there should be 3 jobs.
 	q.showAll()
 
-	print("remove first job: ")
+	print("2. Remove first job.")
+	#remove football
 	q.dequeue()
 
-	print("add job: ")
+	print("3. Add Event: ")
+	#add event
 	concert = Event("U2 gig", "Tomorrow", "8pm", "London")
 	print(concert)
 	q.enqueue(concert)
 
 	#
-	print("first job: ")
-	q.todo()
+	print("4. First job: ")
+	#should be buy milk
+	print(q.todo())
 
-	print("Show all jobs: ")
+	print("5. Show all jobs: ")
+	#should milk, new years, and u2
 	q.showAll()
 
+	print("6. Remove first job.")
+	print("7. Remove first job.")
+	print("8. Remove first job.")
 	q.dequeue()
 	q.dequeue()
 	q.dequeue()
 
-	print("Show all jobs: ")
+	print("9. Show all jobs: ")
+	#should be empty.
 	q.showAll()
 
 if __name__ == '__main__':
